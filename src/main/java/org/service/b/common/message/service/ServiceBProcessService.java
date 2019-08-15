8b17d3693b638ca5f417003dfdf4c4c3d470b136
Jens <jens@ist-ur.org>
2019-08-15 13:59:41 +0200

    set variable boolean and the bpmn

--- src/main/java/org/service/b/common/message/service/ServiceBProcessService.java
+++ src/main/java/org/service/b/common/message/service/ServiceBProcessService.java
@@ -1,5 +1,6 @@
 package org.service.b.common.message.service;
 
+import org.camunda.bpm.engine.runtime.Execution;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 
 import java.util.List;
@@ -9,5 +10,7 @@ public interface ServiceBProcessService {
   List<ProcessInstance> getProcessInstancesByBusinessKey(String businessKey);
 
   ProcessInstance getProcessInstanceByTask(String taskId);
+
+  void setVariable(String executionId, String variableName, Boolean value);
 
 }
