3834079ed85d09609481d061d888d40b2c40a075
Jens <jens@ist-ur.org>
2020-04-01 19:42:29 +0200

    test the j

---
+++ src/test/java/org/service/b/user/UserTest.java
@@ -0,0 +1,14 @@
+package org.service.b.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class UserTest {
+
+  @Test
+  public void getUsers() {
+  }
+
+}
