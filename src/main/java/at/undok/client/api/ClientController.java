fac6edfbec2246062f4cece716fb8d2d44378c4d
jensitus <jens@service-b.org>
2021-06-09 21:43:25 +0200

    first client steps

---
+++ src/main/java/at/undok/client/api/ClientController.java
@@ -0,0 +1,4 @@
+package at.undok.client.api;
+
+public class ClientController {
+}
---
+++ src/main/java/at/undok/client/model/entity/Client.java
@@ -0,0 +1,9 @@
+package at.undok.client.model.entity;
+
+import javax.persistence.Entity;
+
+
+public class Client {
+
+
+}
