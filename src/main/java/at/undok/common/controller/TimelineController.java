78deb2a2457605be3c5fbf66482bbcb9c15d494f
jensitus <jens@service-b.org>
2021-07-23 13:03:38 +0200

    time line controller

--- src/main/java/at/undok/common/controller/TimelineController.java
+++ src/main/java/at/undok/common/controller/TimelineController.java
@@ -2,10 +2,7 @@ package at.undok.common.controller;
 
 import at.undok.common.dto.TimelineItemDto;
 import at.undok.common.message.impl.TimelineService;
-import at.undok.common.message.service.TimelineServiceOldInter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +12,10 @@ import java.util.List;
 @CrossOrigin(origins = {"https://www.service-b.org", "https://service-b.org", "http://localhost:4200", "http://localhost:8080"}, maxAge = 3600)
 @RestController
 @RequestMapping("/timeline")
+@RequiredArgsConstructor
 public class TimelineController {
-  private static final Logger logger = LoggerFactory.getLogger(TimelineController.class);
 
-  @Autowired
-  private TimelineService timelineService;
+  private final TimelineService timelineService;
 
   @GetMapping("/items")
   public List<TimelineItemDto> getItemsByUserId() {
