0fef0894dba320e76ab8286c6497c0bcebbbc771
Gerhard Preisinger <gerhard@radiantracoon.at>
2021-10-19 09:38:13 +0200

    UNDOK-3: Automatically setting spring profile to 'dev' if none was provided

--- src/main/java/at/undok/UndokAt.java
+++ src/main/java/at/undok/UndokAt.java
@@ -3,11 +3,21 @@ package at.undok;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+
 @SpringBootApplication
 public class UndokAt {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UndokAt.class, args);
+		var application = new SpringApplication(UndokAt.class);
+		setDefaultProfileIfNoOverridesExist(application);
+		application.run(args);
+	}
+
+	private static void setDefaultProfileIfNoOverridesExist(SpringApplication application) {
+		var properties = new HashMap<String, Object>();
+		properties.put("spring.profiles.default", "dev");
+		application.setDefaultProperties(properties);
 	}
 
 }
