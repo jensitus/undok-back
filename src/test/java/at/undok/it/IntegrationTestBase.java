1120ce7ece5297645cd74d3312aa9155f14b09fe
Gerhard Preisinger <gerhard@radiantracoon.at>
2021-10-23 22:24:07 +0200

    UNDOK-3: Removing greenmail standalone docker setup for integration testing (using vanilla embedded server instead)

--- src/test/java/at/undok/it/IntegrationTestBase.java
+++ src/test/java/at/undok/it/IntegrationTestBase.java
@@ -10,12 +10,9 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-
-import static at.undok.it.IntegrationTestBase.GreenMailPorts.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @AutoConfigureTestDatabase(replace = Replace.NONE)
@@ -31,18 +28,14 @@ public abstract class IntegrationTestBase {
     private static final boolean SETUP_DOCKER_BASED_TEST_INFRASTRUCTURE = true;
 
     private static final DockerImageName DATABASE_IMAGE_NAME = DockerImageName.parse("postgres:11");
-    private static final DockerImageName GREENMAIL_IMAGE_NAME = DockerImageName.parse("greenmail/standalone:1.6.5");
 
     static final PostgreSQLContainer<?> DATABASE = new PostgreSQLContainer<>(DATABASE_IMAGE_NAME);
-    static final GenericContainer<?> MAIL = new GenericContainer<>(GREENMAIL_IMAGE_NAME)
-            .withExposedPorts(SMTP.port, POP3.port, IMAP.port, SMTPS.port, IMAPS.port, POP3S.port, API.port);
 
     static {
         try {
 
             if (SETUP_DOCKER_BASED_TEST_INFRASTRUCTURE) {
                 DATABASE.start();
-                MAIL.start();
             }
 
         } catch (Throwable e) {
@@ -63,36 +56,6 @@ public abstract class IntegrationTestBase {
         registry.add("spring.datasource.username", DATABASE::getUsername);
         registry.add("spring.datasource.password", DATABASE::getPassword);
         registry.add("spring.jpa.properties.hibernate.dialect", () -> PostgreSQL9Dialect.class.getName());
-    }
-
-    @DynamicPropertySource
-    static void setMailProperties(DynamicPropertyRegistry registry) {
-        if (!SETUP_DOCKER_BASED_TEST_INFRASTRUCTURE) {
-            return;
-        }
-
-        var smtp = MAIL.getMappedPort(SMTP.port);
-        var api = MAIL.getMappedPort(API.port);
-
-        LOGGER.info("Using Greenmail with ports: {} (SMTP), {} (API)", smtp, api);
-
-        registry.add("spring.mail.port", () -> smtp);
-    }
-
-    public enum GreenMailPorts {
-        SMTP(3025),
-        POP3(3110),
-        IMAP(3143),
-        SMTPS(3465),
-        IMAPS(3993),
-        POP3S(3995),
-        API(8080);
-
-        public final int port;
-
-        GreenMailPorts(int port) {
-            this.port = port;
-        }
     }
 
 }
