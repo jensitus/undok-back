69bbe15a4d97e431ff61a0ac2440daaba730e88a
jensitus <jens@service-b.org>
2022-01-09 22:43:35 +0100

    UNDOK-31 reserved words sql replaced

--- src/main/java/at/undok/undok/client/model/entity/ClientEmployer.java
+++ src/main/java/at/undok/undok/client/model/entity/ClientEmployer.java
@@ -21,10 +21,10 @@ public class ClientEmployer extends AbstractCrud {
     @Column(name = "employer_id")
     private UUID employerId;
 
-    @Column(name = "from")
+    @Column(name = "start")
     private LocalDate from;
 
-    @Column(name = "until")
+    @Column(name = "finished")
     private LocalDate until;
 
     @Column(name = "industry")
