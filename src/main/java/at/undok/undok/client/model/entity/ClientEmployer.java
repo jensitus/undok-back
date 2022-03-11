cb5814ccaa47bae139e56a8125a8e1e4c504ebed
jensitus <jens@service-b.org>
2022-03-11 20:39:56 +0100

    UNDOK-4 set some logoutput to reproduce what is going on

--- src/main/java/at/undok/undok/client/model/entity/ClientEmployer.java
+++ src/main/java/at/undok/undok/client/model/entity/ClientEmployer.java
@@ -2,6 +2,7 @@ package at.undok.undok.client.model.entity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "client_employer")
 @Data
+@ToString
 public class ClientEmployer extends AbstractCrud {
 
     @Column(name = "client_id")
