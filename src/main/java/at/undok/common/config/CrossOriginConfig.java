448dd2d4e4e40f7c155d4ab1251b83bd87f50d82
jensitus <jens@service-b.org>
2022-05-16 20:17:46 +0200

    UNDOK-24 maintenance

--- src/main/java/at/undok/common/config/CrossOriginConfig.java
+++ src/main/java/at/undok/common/config/CrossOriginConfig.java
@@ -1,4 +1,4 @@
-package at.undok.common.util;
+package at.undok.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,7 +10,7 @@ public class CrossOriginConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://testprecarioa.undok.at", "http://localhost:4200", "http://localhost:8080", "https://undok.herokuapp.com")
+                .allowedOrigins("https://testprecarioa.undok.at", "http://localhost:4200", "http://localhost:8080", "https://sanprecarioa.undok.at")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .maxAge(3600);
