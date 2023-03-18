0f32501a45301fc15681626fa48f98381056147d
jensitus <jens@service-b.org>
2023-03-18 13:19:46 +0100

    csv from backend

--- src/main/java/at/undok/undok/client/service/CsvService.java
+++ src/main/java/at/undok/undok/client/service/CsvService.java
@@ -2,6 +2,8 @@ package at.undok.undok.client.service;
 
 import at.undok.undok.client.model.dto.AllClientDto;
 import at.undok.undok.client.model.dto.AllCounselingDto;
+import at.undok.undok.client.model.dto.CategoryDto;
+import at.undok.undok.client.util.CategoryType;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Service;
 import java.io.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -21,14 +25,13 @@ public class CsvService {
 
     private final ClientService clientService;
     private final CounselingService counselingService;
+    private final CategoryService categoryService;
 
-    private static final String[] HEADERS = {"firstName", "lastName", "keyword"};
+    private static final String[] CLIENT_HEADERS = {"firstName", "lastName", "keyword"};
 
-
-    @SneakyThrows
-    public void writeClientsToCsv() {
+    public void writeClientsToCsv() throws IOException {
         FileWriter fileOut = new FileWriter("csv/clients.csv");
-        try (CSVPrinter csvPrinter = new CSVPrinter(fileOut, CSVFormat.DEFAULT.withHeader(HEADERS))) {
+        try (CSVPrinter csvPrinter = new CSVPrinter(fileOut, CSVFormat.DEFAULT.withHeader(CLIENT_HEADERS))) {
             List<AllClientDto> all = clientService.getAll();
             for (AllClientDto clientDto : all) {
                 csvPrinter.printRecord(clientDto.getFirstName(), clientDto.getLastName(), clientDto.getKeyword());
@@ -43,15 +46,25 @@ public class CsvService {
         return counselingsToCSV(allCounselings);
     }
 
-    public static ByteArrayInputStream counselingsToCSV(List<AllCounselingDto> counselingDtos) {
-        final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL);
+    private static final String[] COUNSELING_HEADERS = {"id", "Keyword", "concern", "Legal Categories", "activity",
+            "activityCategories", "registeredBy", "counselingDate", "clientFullName", "comment"};
+
+    public ByteArrayInputStream counselingsToCSV(List<AllCounselingDto> counselingDtos) {
+        final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL).withHeader(COUNSELING_HEADERS);
         try (ByteArrayOutputStream out = new ByteArrayOutputStream();
              CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format);) {
             for (AllCounselingDto counselingDto : counselingDtos) {
                 List<String> data = Arrays.asList(
                         String.valueOf(counselingDto.getId()),
                         counselingDto.getKeyword(),
-                        counselingDto.getActivity()
+                        counselingDto.getConcern(),
+                        getCategories(CategoryType.LEGAL, counselingDto.getId()),
+                        counselingDto.getActivity(),
+                        getCategories(CategoryType.ACTIVITY, counselingDto.getId()),
+                        counselingDto.getRegisteredBy(),
+                        counselingDto.getCounselingDate().toString(),
+                        counselingDto.getClientFullName(),
+                        counselingDto.getComment()
                 );
                 csvPrinter.printRecord(data);
             }
@@ -62,8 +75,13 @@ public class CsvService {
         }
     }
 
-    public void writeCounselingsToCsv() {
-
+    private String getCategories(String categoryType, UUID entityId) {
+        List<CategoryDto> categoryListByTypeAndEntity = categoryService.getCategoryListByTypeAndEntity(categoryType, entityId);
+        StringJoiner sj = new StringJoiner(",");
+        for (CategoryDto categoryDto : categoryListByTypeAndEntity) {
+            sj.add(categoryDto.getName());
+        }
+        return sj.toString();
     }
 
 }
