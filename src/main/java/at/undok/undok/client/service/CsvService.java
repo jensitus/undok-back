a9eeca0dfccf798005817ca458a91a6cc4572d40
jensitus <jens@ist-ur.org>
2023-06-14 06:50:47 +0200

    csv service: create and delete csv automatically

--- src/main/java/at/undok/undok/client/service/CsvService.java
+++ src/main/java/at/undok/undok/client/service/CsvService.java
@@ -6,23 +6,29 @@ import at.undok.undok.client.model.dto.AllCounselingDto;
 import at.undok.undok.client.model.dto.CategoryDto;
 import at.undok.undok.client.util.CategoryType;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.QuoteMode;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringJoiner;
-import java.util.UUID;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
+@EnableScheduling
 public class CsvService {
 
     private final ClientService clientService;
@@ -45,55 +51,15 @@ public class CsvService {
             "Aufenthaltsstatus", "Arbeitsmarktzugang", "Position", "Branche", "Gewerkschaft",
             "Mitgliedschaft", "Organization", "Gender"};
 
+    private static final String CSV_DIR = "csv/";
+
     private ByteArrayInputStream writeClientsToCsv(List<AllClientDto> clientDtos) {
         final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL).withHeader(CLIENT_HEADERS);
+
         try (ByteArrayOutputStream out = new ByteArrayOutputStream();
              CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format)) {
             for (AllClientDto clientDto : clientDtos) {
-                String interpreterNecessary = null;
-                if (clientDto.getInterpreterNecessary() != null) {
-                     interpreterNecessary = clientDto.getInterpreterNecessary().toString();
-                }
-                String getVulnerableWhenAssertingRights = null;
-                if (clientDto.getVulnerableWhenAssertingRights() != null) {
-                    getVulnerableWhenAssertingRights = clientDto.getVulnerableWhenAssertingRights().toString();
-                }
-                String getMembership = null;
-                if (clientDto.getMembership() != null) {
-                    getMembership = clientDto.getMembership().toString();
-                }
-                String dateOfBirth = null;
-                if (clientDto.getDateOfBirth() != null) {
-                    dateOfBirth = localDateService.localDateToString(clientDto.getDateOfBirth());
-                }
-                List<String> data = Arrays.asList(
-                        String.valueOf(clientDto.getId()),
-                        clientDto.getKeyword(),
-                        clientDto.getFirstName(),
-                        clientDto.getLastName(),
-                        dateOfBirth,
-                        clientDto.getEmail(),
-                        clientDto.getTelephone(),
-                        clientDto.getStreet(),
-                        clientDto.getZipCode(),
-                        clientDto.getCity(),
-                        clientDto.getCountry(),
-                        clientDto.getEducation(),
-                        clientDto.getMaritalStatus(),
-                        interpreterNecessary,
-                        clientDto.getHowHasThePersonHeardFromUs(),
-                        getVulnerableWhenAssertingRights,
-                        clientDto.getNationality(),
-                        clientDto.getLanguage(),
-                        clientDto.getCurrentResidentStatus(),
-                        clientDto.getLabourMarketAccess(),
-                        clientDto.getPosition(),
-                        clientDto.getSector(),
-                        clientDto.getUnion(),
-                        getMembership,
-                        clientDto.getOrganization(),
-                        clientDto.getGender()
-                );
+                List<String> data = iterateThroughClients(clientDto);
                 csvPrinter.printRecord(data);
             }
             csvPrinter.flush();
@@ -101,6 +67,56 @@ public class CsvService {
         } catch (IOException e) {
             throw new RuntimeException("error while writing csv {}", e);
         }
+    }
+
+    private List<String> iterateThroughClients(AllClientDto clientDto) {
+        List<String> data = null;
+
+        String interpreterNecessary = null;
+        if (clientDto.getInterpreterNecessary() != null) {
+            interpreterNecessary = clientDto.getInterpreterNecessary().toString();
+        }
+        String getVulnerableWhenAssertingRights = null;
+        if (clientDto.getVulnerableWhenAssertingRights() != null) {
+            getVulnerableWhenAssertingRights = clientDto.getVulnerableWhenAssertingRights().toString();
+        }
+        String getMembership = null;
+        if (clientDto.getMembership() != null) {
+            getMembership = clientDto.getMembership().toString();
+        }
+        String dateOfBirth = null;
+        if (clientDto.getDateOfBirth() != null) {
+            dateOfBirth = localDateService.localDateToString(clientDto.getDateOfBirth());
+        }
+        data = Arrays.asList(
+                String.valueOf(clientDto.getId()),
+                clientDto.getKeyword(),
+                clientDto.getFirstName(),
+                clientDto.getLastName(),
+                dateOfBirth,
+                clientDto.getEmail(),
+                clientDto.getTelephone(),
+                clientDto.getStreet(),
+                clientDto.getZipCode(),
+                clientDto.getCity(),
+                clientDto.getCountry(),
+                clientDto.getEducation(),
+                clientDto.getMaritalStatus(),
+                interpreterNecessary,
+                clientDto.getHowHasThePersonHeardFromUs(),
+                getVulnerableWhenAssertingRights,
+                clientDto.getNationality(),
+                clientDto.getLanguage(),
+                clientDto.getCurrentResidentStatus(),
+                clientDto.getLabourMarketAccess(),
+                clientDto.getPosition(),
+                clientDto.getSector(),
+                clientDto.getUnion(),
+                getMembership,
+                clientDto.getOrganization(),
+                clientDto.getGender()
+        );
+        return data;
     }
 
     public ByteArrayInputStream load() {
@@ -141,6 +157,41 @@ public class CsvService {
             sj.add(categoryDto.getName());
         }
         return sj.toString();
+    }
+
+    @SneakyThrows
+    @Scheduled(cron = "${csv-backup.schedule}")
+    public void csvAsBackup() {
+        deleteOldCSVs();
+        log.info("- - - - - start csv backup - - - - -");
+        String fileName = LocalDateTime.now() + "-clients.csv";
+        FileWriter fileOut = new FileWriter(CSV_DIR + fileName);
+        try (CSVPrinter csvPrinter = new CSVPrinter(fileOut, CSVFormat.DEFAULT.withHeader(CLIENT_HEADERS))) {
+            List<AllClientDto> all = clientService.getAll();
+            for (AllClientDto clientDto : all) {
+                List<String> data = iterateThroughClients(clientDto);
+                csvPrinter.printRecord(data);
+            }
+        } catch (IOException e) {
+            log.error("error while writing csv {}", e.toString());
+        }
+    }
+
+    @SneakyThrows
+    private Set<String> deleteOldCSVs() {
+        Set<String> stringSet =
+                Stream.of(Objects.requireNonNull(new File("csv/").listFiles()))
+                      .filter(file -> !file.isDirectory()).map(File::getName).collect(Collectors.toSet());
+        for (String fileName : stringSet) {
+            String[] split = fileName.split("T");
+            LocalDate localDateTime = LocalDate.parse(split[0]);
+            if (localDateTime.isBefore(LocalDate.now().minusDays(7))) {
+                Path toDelete = Paths.get(CSV_DIR + fileName);
+                log.info("CSV {} to delete", toDelete.toString());
+                Files.delete(toDelete);
+            }
+        }
+        return stringSet;
     }
 
 }
